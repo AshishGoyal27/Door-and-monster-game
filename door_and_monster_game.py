6484867ccb39d872c33d81bb9d73287e26fdd913
Ashish Goyal <49943219+AshishGoyal27@users.noreply.github.com>
2020-06-05 01:16:25 +0530

    Add files via upload

---
+++ door_and_monster_game.py
@@ -0,0 +1,47 @@
+import random
+def doors():
+  print(''' 
+_____1_____    _____2____   _____3_____
+|  __  __  |  |  __  __  |  |  __  __  |
+| |  ||  | |  | |  ||  | |  | |  ||  | |
+| |  ||  | |  | |  ||  | |  | |  ||  | |
+| |__||__| |  | |__||__| |  | |__||__| |
+|  __  __()|  |  __  __()|  |  __  __()|
+| |  ||  | |  | |  ||  | |  | |  ||  | |
+| |  ||  | |  | |  ||  | |  | |  ||  | |
+| |  ||  | |  | |  ||  | |  | |  ||  | |
+| |  ||  | |  | |  ||  | |  | |  ||  | |
+| |__||__| |  | |__||__| |  | |__||__| |
+|__________|  |__________|  |__________|''')
+
+def monster():
+	print('''
+        .-""-"-""-.
+       | .--.-.--. |
+       |` >       `|
+       | <         |
+       (__..---..__)
+      (`|\o_/ \_o/|`)
+       \(    >    )/
+     [>=|   vvv   |=<]
+        \__\   /__/
+            '-'
+ ''')
+print('Halloween Game')
+
+print('Avoid the monster!')
+monsterDoor = random.randint(1, 3)
+print('Three doors ahead...')
+def game():
+    doors()
+    print('Is the Monster behind door..')
+    userDoor = int(input ('1,2, or 3?'))
+    if userDoor ==  monsterDoor:
+        print(' ARGHHH! MONSTER')
+        monster()
+    else:
+        print('Phew! No Monster!')
+        print('You enter the next room. ')
+        print('Again Three doors ahead')
+        game()
+game()
